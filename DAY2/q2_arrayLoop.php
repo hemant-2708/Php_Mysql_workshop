f3c150813f85937ecc974fc21a4a2e7cb3d5932d
hemant-2708 <hr21354@gmail.com>
2020-10-01 14:49:37 +0530

    Day 2 assignment.

---
+++ DAY2/q2_arrayLoop.php
@@ -0,0 +1,9 @@
+<?php
+
+$x = array(1 => 'one', 2 => 'two', 3 => 'three');
+
+foreach ($x as $key => $value){
+	echo $key.". ".$value."<br>";
+}
+
+?>
---
+++ DAY2/q4_function2.php
@@ -0,0 +1,10 @@
+<?php
+
+function writeName2($firstname){
+	return $firstname;
+}
+
+echo "My name is ".writeName2('Jai')." Sharma.<br>";
+echo "My brother's name is ".writeName2('Ram')." Sharma.";
+
+?> 
